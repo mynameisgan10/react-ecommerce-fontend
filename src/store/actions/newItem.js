fc99feb5152e67538784e05f26be3f980470f17a
Gan <geekwithgun@gmail.com>
2018-09-23 21:28:48 +0700

    set up redux to list new item

--- src/store/actions/newItem.js
+++ src/store/actions/newItem.js
@@ -38,3 +38,21 @@ export const select_item_category = (cat_id) => {
 export const select_item_condition = (value) => {
     return {type: actions.SELECT_ITEM_CONDITION,value: value}
 }
+
+export const list_new_item = () => {
+    return {
+        type: actions.LIST_NEW_ITEM
+    }
+}
+
+const list_new_item_success = () => {
+    return {
+        type: actions.LIST_NEW_ITEM_SUCCESS
+    }
+}
+
+const list_new_item_fail = () => {
+    return{
+        type: actions.LIST_NEW_ITEM_FAIL
+    }
+}
